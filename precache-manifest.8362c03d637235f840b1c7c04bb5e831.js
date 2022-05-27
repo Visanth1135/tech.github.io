6885dc6024bc72188220d6da225a97b05abecd1a
Visanth1135 <k.visanth1996@gmail.com>
2022-05-27 20:17:13 +0400

    Updates

---
+++ precache-manifest.8362c03d637235f840b1c7c04bb5e831.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4bf98ca4170c9a29d9cef70250a924",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65c6ee61af7e1cc4282f",
+    "url": "/static/css/2.ef07fde4.chunk.css"
+  },
+  {
+    "revision": "5503095d20805ed327b1",
+    "url": "/static/css/main.cfa9a568.chunk.css"
+  },
+  {
+    "revision": "65c6ee61af7e1cc4282f",
+    "url": "/static/js/2.ac1c1dde.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/static/js/2.ac1c1dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5503095d20805ed327b1",
+    "url": "/static/js/main.923bf183.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
